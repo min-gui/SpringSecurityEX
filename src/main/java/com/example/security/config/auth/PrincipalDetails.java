aae2f4c57f39b8517f3974e50156f70fdb594abd
min-gui <msj349911@gmail.com>
2023-07-10 23:48:55 +0900

    권한 주기

--- src/main/java/com/example/security/config/auth/PrincipalDetails.java
+++ src/main/java/com/example/security/config/auth/PrincipalDetails.java
@@ -1,2 +1,67 @@
-package com.example.security.config.auth;public class PrincipalDetails {
+package com.example.security.config.auth;
+
+// 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다.
+// 로그인을 진행이 완료가 되면 시큐리티 session을 만들어줍니다. (Security ContextHolder)
+// 오브젝트 => Authentication 타입 객체
+// Authentication 안에 User정보가 있어야 됨.
+// User오브젝트타입 => UserDetails 타입 객체
+
+//Security Session==> Authentication => UserDetails(PrincipalDetails)
+
+import com.example.security.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class PrincipalDetails implements UserDetails {
+
+    private final User user;//콤포지션
+
+    //해당 User의 권한을 리턴하는 곳
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole();
+            }
+        });
+
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
